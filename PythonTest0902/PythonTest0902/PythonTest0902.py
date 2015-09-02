fbb573492c9faed7342b9d06b5a3c2fe8118464c
Freedate <kod9326@gmail.com>
2015-09-02 13:46:07 +0900

    Test0902

---
+++ PythonTest0902/PythonTest0902/PythonTest0902.py
@@ -0,0 +1,53 @@
+﻿#print("Here is the numbers!" + \
+#    "The first is{0:d} The second is{1:4d}".format(7,8))
+
+#salary = input("Please enter your salary : ")
+#bonus = input("Please enter your bonus : ")
+#payCheck = salary + bonus
+#payCheck = float(salary) + float(bonus)
+#print(payCheck)
+#print(type(payCheck))
+
+#print('한글')
+
+#print('''첫번째줄
+#두번째줄
+#세번째줄
+#''')
+
+#print("="*10)
+#name="greenjoa"
+#print(name[0:8])
+#print(name[0:-3])
+#print(name[-2])
+
+#info="201111305Freedate"
+#sid=info[:9]
+#sname=info[9:]
+#print(sid+" "+sname)
+
+#a = 'pithon'
+#a = a[:1]+'y'+a[2:]
+#print(a)
+
+#a="I eat %10s apples" % "five"
+#print(a)
+#b="I eat %-10s apples" % "five"
+#print(b)
+
+#c = "Error is %d%%." %98
+#print(c)
+
+
+#message = 'hello world'
+#print(message.lower())
+#print(message.upper())
+#print(message.swapcase())
+#print(message.find('world'))
+#print(message.index('o'))
+#print(message.count('o'))
+#print(message.capitalize())
+#print(message.replace('hello', 'Hi'))
+
+a = input("종료할까요? ")
+print(a.lower())
