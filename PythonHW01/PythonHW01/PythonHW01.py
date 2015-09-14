6fa5b358008f31e61f2f7e10ec67f152f9e00a0b
Freedate <kod9326@gmail.com>
2015-09-14 20:27:53 +0900

    Update PythonHW01.py

--- PythonHW01/PythonHW01/PythonHW01.py
+++ PythonHW01/PythonHW01/PythonHW01.py
@@ -1,36 +1,53 @@
-﻿import turtle
+import turtle
 import random
 
-turtle.shape("turtle")
+turtlea = turtle.Turtle()
 
+turtlea.shape("turtle")
 str = "201111305 이동호 Python Turtle Test"
+
 turtle.title(str);
+turtlea.color("red")
+turtlea.penup()
+turtlea.forward(100)
+turtlea.pendown()
+turtlea.right(270)
+turtlea.begin_fill()
+turtlea.circle(100)
+turtlea.end_fill()
+#turtlea.penup()
 
-turtle.color("red")
+#################################여기까지 과제 1-1
 
-turtle.forward(100)
-turtle.right(270)
+turtleb = turtle.Turtle()
+turtleb.goto(0,0)
+turtleb.color("green")
+turtleb.shape("turtle")
 
-turtle.begin_fill()
-turtle.circle(100)
-turtle.end_fill()
+turtlea.color("blue")
+turtlea.circle(100)
+turtlea.goto(0,0)
 
-
-turtle.circle(100)
-
-turtle.goto(0,0)
-turtle.color("yellow")
-
-str2 = turtle.distance(0,0)
+str2 = turtlea.distance(0,0)
 print(str2)
+while turtlea.distance(0,0)<=100 :
+    turtlea.forward(10)
+    turtleb.forward(10)
 
-while turtle.distance(0,0)<=100 :
-    turtle.forward(1)
-    if turtle.distance(0,0)>100 :
-        turtle.backward(1)
+    if turtlea.distance(0,0)>100 :
+        turtlea.backward(10)
         a = random.randint(90,270)
-        turtle.right(a)
-        
+        turtlea.right(a)
+    
+    if turtleb.distance(0,0)>100:
+        turtleb.backward(10)
+        b = random.randint(90,270)
+        turtleb.right(b)
 
 
-turtle.exitonclick()
+
+#################################여기까지 과제 1-2
+
+
+
+#################################여기까지 추가구현
